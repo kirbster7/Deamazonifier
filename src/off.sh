06e446de5a6ae5e88d8d7eb9f4aa116908f426f7
kirbster7 <90074338+kirbster7@users.noreply.github.com>
2022-12-30 19:18:43 -0700

    Initial Source Upload
    Excluded APK files

---
+++ src/off.sh
@@ -0,0 +1,62 @@
+clear
+echo "// Welcome to Deamazonifier v1.0"
+sleep 1 
+echo "// Starting script in 5 seconds... (ctrl+c to abort)"
+sleep 5
+
+# uninstall Gapps
+echo "// Deamazonifier is removing Google Play services"
+echo "Removing Google Play Store Frontend (1/4)"
+./adb uninstall com.android.vending
+echo "Removing Google Play Service (2/4)"
+./adb uninstall com.google.android.gms
+echo "Removing Google Play Framework (3/4)"
+./adb uninstall com.google.android.gsf
+echo "Removing Google Account Manager (4/4)"
+./adb uninstall com.google.android.gsf.login
+
+# disable amazon apps
+echo "// Deamazonifier is re-enabling Amazon apps"
+packages=$(awk '{print $1}' < amazonlist.txt)
+./adb shell pm list packages -s > packagelist
+for package in ${packages}
+do
+    ./adb shell pm enable "$package"
+done
+
+# change settings
+echo "// Deamazonifier is reverting your devices settings"
+./adb shell settings put secure limit_ad_tracking 0
+./adb shell settings put secure usage_metrics_marketing_enabled 1
+./adb shell settings put secure USAGE_METRICS_UPLOAD_ENABLED 1
+./adb shell settings put global private_dns_mode -hostname
+./adb shell settings put global LOCKSCREEN_AD_ENABLED 1
+./adb shell settings put secure search_on_lockscreen_settings 1
+./adb shell settings put global always_finish_activities 0
+./adb shell settings put global window_animation_scale 1
+./adb shell settings put global transition_animation_scale 1
+./adb shell settings put global animator_duration_scale 1
+
+# remove google alts
+echo "// Deamazonifier is removing apps"
+echo "Removing Google Chrome (1/2)"
+./adb uninstall com.android.chrome
+echo "Removing Google Gallery (2/2)"
+./adb uninstall com.google.android.apps.photosgo
+
+# remove lawnchair
+echo "// Deamazonifier is reverting the launcher"
+./adb shell pm enable com.amazon.firelauncher
+./adb shell am start com.amazon.firelauncher
+echo "Removing Lawnchair (1/1)"
+./adb uninstall ch.deletescape.lawnchair.ci
+
+# finish up
+echo "// Please wait"
+sleep 5
+clear
+echo "// Rebooting your device in 15 seconds"
+sleep 15
+./adb reboot
+clear
+echo "// Deamazonifier is finished."
---
+++ src/on.sh
@@ -0,0 +1,65 @@
+clear
+echo "// Welcome to Deamazonifier v1.0"
+sleep 1 
+echo "// Starting script in 5 seconds... (ctrl+c to abort)"
+sleep 5
+
+# install Gapps
+echo "// Deamazonifier is installing Google Play services"
+echo "Installing Google Account Manager (1/4)"
+./adb install Gapps/am.apk
+echo "Installing Google Play Framework (2/4)"
+./adb install Gapps/fw.apk
+echo "Installing Google Play Service (3/4)"
+./adb install Gapps/ps.apk
+echo "Installing Google Play Store Frontend (4/4)"
+./adb install Gapps/store.apk
+
+# disable amazon apps
+echo "// Deamazonifier is disabling Amazon apps"
+packages=$(awk '{print $1}' < amazonlist.txt)
+./adb shell pm list packages -s > packagelist
+for package in ${packages}
+do
+    ./adb shell pm disable-user --user 0 "$package"
+done
+
+# change settings
+echo "// Deamazonifier is changing your devices settings"
+./adb shell settings put secure limit_ad_tracking 1
+./adb shell settings put secure usage_metrics_marketing_enabled 0
+./adb shell settings put secure USAGE_METRICS_UPLOAD_ENABLED 0
+./adb shell pm clear com.amazon.advertisingidsettings
+./adb shell settings put secure location_providers_allowed -network
+./adb shell settings put global private_dns_mode hostname
+./adb shell settings put global private_dns_specifier dns.adguard.com
+./adb shell settings put global LOCKSCREEN_AD_ENABLED 0
+./adb shell settings put secure search_on_lockscreen_settings 0
+./adb shell settings put global window_animation_scale 0.50
+./adb shell settings put global transition_animation_scale 0.50
+./adb shell settings put global animator_duration_scale 0.50
+
+# replace amazon apps with google alternatives
+echo "// Deamazonifier is installing Google Alternatives to deleted apps"
+echo "Installing Google Chrome (1/2)"
+./adb install GoogleAlts/chrome.apk #chrome
+echo "Installing Google Gallery (2/2)"
+./adb install GoogleAlts/gallery.apk
+
+# install lawnchair
+echo "// Deamazonifier is replacing the launcher"
+echo "Installing Lawnchair (1/1)"
+./adb install Launchers/lawnchair.apk
+./adb shell am start ch.deletescape.lawnchair.ci
+./adb shell pm disable-user --user 0 com.amazon.firelauncher
+
+# finish up
+echo "// Please wait"
+sleep 5
+clear
+echo "// Please Press 'Allow' to the popup"
+echo "// Rebooting your device in 15 seconds"
+sleep 15
+./adb reboot
+clear
+echo "// Deamazonifier is finished, enjoy your newly debloated device!"
